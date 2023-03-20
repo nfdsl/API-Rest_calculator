ab0808f3066b9e546358549b4782dcec394f2367
Natan <nfsl@cin.ufpe.br>
2023-03-20 10:49:52 -0300

    Criação Projeto

---
+++ src/main/java/APIproj/com/br/APIRest_calculator/ApiRestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package APIproj.com.br.APIRest_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/APIproj/com/br/APIRest_calculator/ApiRestCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package APIproj.com.br.APIRest_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
